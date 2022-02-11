961c4abd00f8fd198107e8959d8f27700a982d1d
David Alasow <davidalasow@gmail.com>
2022-02-11 18:20:05 +0100

    Add method for getting hotspot by name.

--- src/hotspots.rs
+++ src/hotspots.rs
@@ -6,10 +6,16 @@ pub fn all(client: &Client) -> Stream<Hotspot> {
 }
 
 /// Get a specific hotspot by its address
-pub async fn get(client: &Client, address: &str) -> Result<Hotspot> {
+pub async fn get_address(client: &Client, address: &str) -> Result<Hotspot> {
     client
         .fetch(&format!("/hotspots/{}", address), NO_QUERY)
         .await
+}
+/// Get a specific hotspot by the 3-word animal name.
+pub async fn get_name(client: &Client, name: &str) -> Result<Vec<Hotspot>> {
+	client
+		.fetch(&format!("/hotspots/name/{}", name), NO_QUERY)
+		.await
 }
 
 #[cfg(test)]
@@ -31,7 +37,7 @@ mod test {
     #[test]
     async fn get() {
         let client = Client::default();
-        let hotspot = hotspots::get(
+        let hotspot = hotspots::get_address(
             &client,
             "112vvSrNAwJRSmR54aqFLEhbr6cy6T4Ufuja4VWVrxvkUAUxL2yG",
         )
